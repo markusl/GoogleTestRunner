3f7fa2c3282e49b485d9a4e92f3ccd43e8d1a6a2
Markus Lindqvist <markus.lindqvist@iki.fi>
2013-11-21 10:32:14 +0200

    Fix test to work on W8;)

--- GoogleTestRunnerTests/PeTest.fs
+++ GoogleTestRunnerTests/PeTest.fs
@@ -12,4 +12,10 @@ type ``Test PeParser`` ()=
     member x.``read imports`` ()=
         let pe = Native.PeParser(path)
         let imports = pe.Imports()
-        imports.Length |> should equal 24
+        let v = Environment.OSVersion.Version
+        // W7
+        if v.Major = 6 && v.Minor = 1 then
+            imports.Length |> should equal 24
+        // W8
+        if v.Major = 6 && v.Minor = 2 then
+            imports.Length |> should equal 49
